3511600615f86333548bc37d9225240765b13f19
Alex <rengarsanj@gmail.com>
2023-03-28 18:52:04 +0300

    before critical changes

--- src/components/addProduct/AddProduct.jsx
+++ src/components/addProduct/AddProduct.jsx
@@ -1,4 +1,6 @@
 
+import { useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 import PageFooter from '../pageFooter/PageFooter';
@@ -6,33 +8,58 @@ import PageFooter from '../pageFooter/PageFooter';
 import './addProduct.scss';
 
 const AddProduct = () => {
+
+  const [inputs, setInputs] = useState();
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost/api/user/save', inputs).then(function(response){
+    console.log(response.data);
+    
+    });
+    // console.log(inputs);
+  }
+
+  
+
+
   return (
     <>
       <div className="header">
         <div className="header__wrapper">
           <h2>Product Add</h2>
           <div className="header__buttons">
-            <button className='header__button'>Save</button>
+            <button type="submit" form="#product_form" className='header__button'>Save</button>
             <Link to="/" className='header__button'>Cancel</Link>
           </div>
         </div>
         <div className="header__divider"></div>
       </div>
 
-      <form action="">
+      {/* <form action=""> */}
+      <form id='#product_form' onSubmit={handleSubmit}>
         <label htmlFor="#sku">SKU</label>
-        <input id='#sku' name='#sku' type="text" placeholder="#sku" />
-
+        <input id='#sku' name='sku' onChange={handleChange} type="text" placeholder="#sku" />
+        <br />
+        <br />
         <label htmlFor="#name">Name</label>
-        <input id='#name' name='#name' type="text" placeholder="#name" />
-
+        <input id='#name' name='name' onChange={handleChange} type="text" placeholder="#name" />
+        <br />
+        <br />
         <label htmlFor="#price">Price ($)</label>
-        <input id='#price' name='#price' type="text" placeholder="#price" />
+        <input id='#price' name='price' onChange={handleChange} type="text" placeholder="#price" />
 
-        <label htmlFor="#productType">Type Switcher</label>
+        {/* <label htmlFor="#productType">Type Switcher</label>
         <select id='#productType' name='#productType'>
           <option value="one">one</option>
-        </select>
+        </select> */}
       </form>
 
       <PageFooter/>
--- src/components/productListItem/ProductListItem.jsx
+++ src/components/productListItem/ProductListItem.jsx
@@ -2,15 +2,26 @@
 
 import './productListItem.scss';
 
-const ProductListItem = () => {
+const ProductListItem = (props) => {
+ const {id, sku, name, price} = props.product
+ const deleteUser = props.deleteUser
+
+
   return (
     <div className="product-list-item">
-      <input type="checkbox" name="checkbox"></input>
+      <input onClick={() => deleteUser(id)} className='product-list-item__checkbox' type="checkbox" name="checkbox"></input>
       <div className="product-list-item__wrapper">
-        <h2>Hello ProductListItem</h2>
+        <div className="product-list-item__content">
+          <p>{id}</p>
+          <p>{sku}</p>
+          <p>{name}</p>
+          <p>{price}</p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default ProductListItem;
+
+
