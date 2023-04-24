603e8c55452f2b56a1ea368cd84b472a20ec17b0
Alex <rengarsanj@gmail.com>
2023-04-24 15:58:32 +0300

    PHP to OOP

--- api/index.php
+++ api/index.php
@@ -4,40 +4,76 @@ header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: *");
 
-// echo "<script>console.log('Testing' );</script>";
-// echo "Testing";
-
 include 'DbConnect.php';
-$objDb = new DbConnect;
-$conn = $objDb->connect();
-// var_dump($conn);
 
-// $user = print_r(file_get_contents('php://input'));
-$method = $_SERVER['REQUEST_METHOD'];
-switch($method) {
-  case "GET":
+class Product {
+  public $sku;
+  public $name;
+  public $price;
+
+  public function __construct($sku, $name, $price) {
+    $this->sku = $sku;
+    $this->name = $name;
+    $this->price = $price;
+  }
+}
+
+class DVD extends Product {
+  public $size;
+
+  public function __construct($sku, $name, $price, $size) {
+    parent::__construct($sku, $name, $price);
+    $this->size = $size;
+  }
+}
+
+class Book extends Product {
+  public $weight;
+
+  public function __construct($sku, $name, $price, $weight) {
+    parent::__construct($sku, $name, $price);
+    $this->weight = $weight;
+  }
+}
+
+class Furniture extends Product {
+  public $height;
+  public $width;
+  public $length;
+
+  public function __construct($sku, $name, $price, $height, $width, $length) {
+    parent::__construct($sku, $name, $price);
+    $this->height = $height;
+    $this->width = $width;
+    $this->length = $length;
+  }
+}
+
+class ProductController {
+  private $conn;
+
+  public function __construct($conn) {
+    $this->conn = $conn;
+  }
+
+  public function getAllProducts() {
     $sql = "SELECT * FROM products";
-    $path = explode('/', $_SERVER['REQUEST_URI']);
-    if(isset($path[3]) && is_numeric($path[3])) {
-      $sql .= " WHERE id = :id";
-      $stmt = $conn->prepare($sql);
-      $stmt->bindParam(':id', $path[3]);
-      $stmt->execute();
-      $products = $stmt->fetch(PDO::FETCH_ASSOC);
-    } else {
-      $stmt = $conn->prepare($sql);
-      $stmt->execute();
-      $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    echo json_encode($products);
-    break;
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
 
-  case "POST": 
+  public function getProductById($id) {
+    $sql = "SELECT * FROM products WHERE id = :id";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+  }
 
-
-    $product = json_decode( file_get_contents('php://input') );
+  public function addProduct($product) {
     $sql = "INSERT INTO products(id, sku, name, price, size, height, width, length, weight) VALUES(null, :sku, :name, :price, :size, :height, :width, :length, :weight)";
-    $stmt = $conn->prepare($sql);
+    $stmt = $this->conn->prepare($sql);
     $stmt->bindParam(':sku', $product->sku);
     $stmt->bindParam(':name', $product->name);
     $stmt->bindParam(':price', $product->price);
@@ -46,26 +82,50 @@ switch($method) {
     $stmt->bindParam(':width', $product->width);
     $stmt->bindParam(':length', $product->length);
     $stmt->bindParam(':weight', $product->weight);
-    if ($stmt->execute()) {
+    return $stmt->execute();
+  }
+
+  public function deleteProduct($id) {
+    $sql = "DELETE FROM products WHERE id = :id";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    return $stmt->execute();
+  }
+}
+
+$objDb = new DbConnect;
+$conn = $objDb->connect();
+$method = $_SERVER['REQUEST_METHOD'];
+$productController = new ProductController($conn);
+
+switch ($method) {
+  case "GET":
+    $path = explode('/', $_SERVER['REQUEST_URI']);
+    if (isset($path[3]) && is_numeric($path[3])) {
+      $products = $productController->getProductById($path[3]);
+    } else {
+      $products = $productController->getAllProducts();
+    }
+    echo json_encode($products);
+    break;
+
+  case "POST":
+    $product = json_decode(file_get_contents('php://input'));
+    if ($productController->addProduct($product)) {
       $response = ['status' => 1, 'message' => 'Record created successfully.'];
     } else {
-      $response = ['status' => 0, 'message' => 'Failed to created record!'];
+      $response = ['status' => 0, 'message' => 'Failed to create record!'];
     }
+    echo json_encode($response);
     break;
 
   case "DELETE":
-    $sql = "DELETE FROM products WHERE id = :id";
     $path = explode('/', $_SERVER['REQUEST_URI']);
-
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':id', $path[3]);
-
-    if($stmt->execute()) {
-        $response = ['status' => 1, 'message' => 'Record deleted successfully.'];
+    if ($productController->deleteProduct($path[3])) {
+      $response = ['status' => 1, 'message' => 'Record deleted successfully.'];
     } else {
-        $response = ['status' => 0, 'message' => 'Failed to delete record.'];
+      $response = ['status' => 0, 'message' => 'Failed to delete record.'];
     }
     echo json_encode($response);
     break;
 }
-
